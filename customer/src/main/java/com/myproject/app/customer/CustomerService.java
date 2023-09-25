bf7f8529df96bdd6cd9c305b86e8e040dc46c8a7
Phan Van Tiep <84904064+PVanTiep@users.noreply.github.com>
2023-09-25 19:48:19 +0700

    git commit

--- customer/src/main/java/com/myproject/app/customer/CustomerService.java
+++ customer/src/main/java/com/myproject/app/customer/CustomerService.java
@@ -1,9 +1,15 @@
 package com.myproject.app.customer;
 
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+@AllArgsConstructor
+public class CustomerService {
+    private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
@@ -12,6 +18,16 @@ public record CustomerService(CustomerRepository customerRepository) {
                 .build();
         // todo: check if email valid
         //  check if email already exists
-        customerRepository.save(customer);
+        //  check fraudster
+        customerRepository.saveAndFlush(customer);
+        FraudCheckResponse fraudCheckResponse =  restTemplate.getForObject(
+                "http://FRAUD/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+        );
+        if (fraudCheckResponse==null && fraudCheckResponse.isFraudster()) {
+            throw new IllegalStateException("fraudster");
+        }
+        // todo: send notification
     }
 }
